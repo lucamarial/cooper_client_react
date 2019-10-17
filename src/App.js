57a4010abd108bba60ff0e733d3ce6ebfb171352
luca <l.lobacher@web.de>
2019-10-17 11:16:56 +0200

    Adds component displayCooperResult
    Adds a CooperCalculator Module.
    Adds feature test.
    Adds unit test to test the CooperResult.
    Deletes unnecessary files.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <label>Distance</label>
+          <input id="distance"></input>
+        </div>
+
+        <select id="gender">
+          <option value="female">Female</option>
+          <option value="male">Male</option>
+        </select>
+
+        <div>
+          <label>Age</label>
+          <input id="age"></input>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
