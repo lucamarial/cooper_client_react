8572e2107f1202ae70fcbb770dc2380e3d3c1bd8
luca <l.lobacher@web.de>
2019-10-17 22:48:20 +0200

    Retrieving data
    Adds stubbed feature test that userCanSeeIndexOfSavedPerformanceData
    Adds fixture file for feature test
    Adds 'Show past entries' button to App.js

--- src/App.js
+++ src/App.js
@@ -44,11 +44,15 @@ class App extends Component {
   render() {
     let renderLogin;
     let user;
+    let performanceDataIndex;
 
     if (this.state.authenticated === true) {
       user = JSON.parse(sessionStorage.getItem('credentials')).uid;
       renderLogin = (
         <p>Hi {user}</p>
+      )
+      performanceDataIndex = (
+        <button id="show-index" onClick={() => this.setState({ renderIndex: true })}>Show past entries</button>
       )
     } else { 
       if (this.state.renderLoginForm === true) {
@@ -84,6 +88,7 @@ class App extends Component {
           entrySaved = { this.state.entrySaved }
           entryHandler = { this.entryHandler.bind(this) }
         />
+        {performanceDataIndex}
         <div>
         { renderLogin }
         </div>
