6addaa5d2d9b8dc02c4f9b531a380eee5a6b15e5
luca <l.lobacher@web.de>
2019-10-17 21:27:02 +0200

    Creates performance data module, addssave button to save performance data in DisplayCooperResult component.

--- src/Components/DisplayCooperResult.js
+++ src/Components/DisplayCooperResult.js
@@ -7,14 +7,35 @@ class DisplayCooperResult extends Component {
     return CooperCalculator(this.props.distance, this.props.gender, this.props.age);
   }
 
+  async saveCooperData() {
+    const result = this.calculate();
+    try {
+      await this.saveData(result);
+      this.props.entryHandler();
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
   render() {
-    let results
+    let results;
+    let saveButton;
+
+    if (this.props.authenticated === true) {
+      saveButton = (
+        <>
+          <button id="save-result" onClick={this.saveCooperData.bind(this)}>Save entry</button>
+        </>
+      )
+    }
+
     if (this.props.age !== '' && this.props.distance !== '') {
       results =
-        <div>
+        <>
           <p>{this.props.age} y/o {this.props.gender} running {this.props.distance} meters.</p>
           <p>Result: {this.calculate()}</p>
-        </div>
+          {saveButton}
+        </>
     }
     return (
       <div>
